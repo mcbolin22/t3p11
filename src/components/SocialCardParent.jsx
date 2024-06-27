8fb3878d9b446fd6d592b58ae6bf5a9565cfaecc
Colin <mcbolin@hotmail.com>
2024-06-27 18:26:02 +1000

    feat: parent structure in place

---
+++ src/components/SocialCardParent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SocialCardForm from "./SocialCardForm";
+import SocialCardDisplay from "./SocialCardDisplay";
+
+export default class SocialCardParent extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            currentlyEditing: false,
+            content: "Default Content",
+            imageUrl: "",
+
+
+        }
+    }
+
+    render(){
+
+        if (this.state.currentlyEditing){
+            return <SocialCardForm content={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
+        } else {
+            return <SocialCardDisplay content={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
+        }
+    }
+}
